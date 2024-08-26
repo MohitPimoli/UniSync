db52f91b814ac5d5fe85ded9b16a5e6ba2377c08
nikhil <nb9690@gmail.com>
2024-08-26 16:26:34 +0530

    forget password page created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './component/LandingPage/LandingPage';
 import Login from './component/LoginPage/LoginPage';
+import Forget from './component/LoginPage/ForgetPassword'
 function App() {
   return (
     <div className="Pages">
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/forget" element={<Forget />} />
         </Routes>
       </BrowserRouter>
     </div>
